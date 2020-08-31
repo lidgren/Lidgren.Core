c72594c5b797773ef61a15cc8f30c93f457a8f27
Michael Lidgren <lidgren@gmail.com>
2020-08-31 19:24:52 +0200

    DynamicOrdering added

---
+++ DynamicOrdering.cs
@@ -0,0 +1,223 @@
+﻿using System;
+
+namespace Lidgren.Core
+{
+	public enum DynamicOrder
+	{
+		RequireABeforeB,
+		PreferABeforeB,
+		DoesNotMatter,
+		PreferBBeforeA,
+		RequireBBeforeA,
+	}
+
+	public interface IDynamicOrdered<TItem>
+	{
+		DynamicOrder OrderAgainst(TItem other);
+	}
+
+	/// <summary>
+	/// Sort ordering of items making individual ordering requests
+	/// </summary>
+	public static class DynamicOrdering<TItem>
+	{
+		public static TItem[] Perform<TDynItem>(ReadOnlySpan<TItem> items) where TDynItem : IDynamicOrdered<TItem>
+		{
+			return Perform(items, Compare);
+		}
+
+		private static DynamicOrder Compare(TItem a, TItem b)
+		{
+			IDynamicOrdered<TItem> da = (IDynamicOrdered<TItem>)a;
+			return da.OrderAgainst(b);
+		}
+
+		public static TItem[] Perform(ReadOnlySpan<TItem> items, Func<TItem, TItem, DynamicOrder> orderFunc)
+		{
+			int len = items.Length;
+
+			var order = new FastList<int>(len);
+
+			// first; determine number of dependencies for each item; to use as subscore when ordering
+			var dependenciesPerItem = new int[len];
+			for (int aidx = 0; aidx < len; aidx++)
+			{
+				for (int bidx = 0; bidx < len; bidx++)
+				{
+					if (aidx == bidx)
+						continue;
+					var cmp = GetOrder(orderFunc, items[aidx], items[bidx]);
+					if (cmp == DynamicOrder.RequireABeforeB)
+						dependenciesPerItem[bidx]++;
+				}
+			}
+
+			for (int insertOriginalIndex = 0; insertOriginalIndex < len; insertOriginalIndex++)
+			{
+				if (order.Count == 0)
+				{
+					order.Add(insertOriginalIndex);
+					continue;
+				}
+
+				ref readonly var item = ref items[insertOriginalIndex];
+
+				var myDeps = dependenciesPerItem[insertOriginalIndex];
+
+				// find best insertion point for this item
+				int bestInsertionIndex = -1;
+				int bestScore = -1;
+				int bestPrecedingDeps = -1;
+
+				//
+				// test all insertion points; which is the best one
+				//
+				for (int testInsertIndex = 0; testInsertIndex < order.Count + 1; testInsertIndex++) // +1 to test insertion point efter all items
+				{
+					int score = 0;
+					int numPrecedingDeps = 0;
+					bool invalid = false;
+
+					// score by those that would come before this insertion point
+					for (int b = 0; b < testInsertIndex; b++)
+					{
+						int beforeIndex = order[b];
+						var cmp = GetOrder(orderFunc, item, items[beforeIndex]);
+						switch (cmp)
+						{
+							case DynamicOrder.RequireABeforeB:
+								invalid = true;
+								break;
+							case DynamicOrder.DoesNotMatter:
+								break;
+							case DynamicOrder.PreferABeforeB:
+								score--;
+								break;
+							case DynamicOrder.RequireBBeforeA:
+							case DynamicOrder.PreferBBeforeA:
+								score++;
+								break;
+						}
+						numPrecedingDeps += dependenciesPerItem[beforeIndex];
+					}
+					if (invalid)
+						continue;
+
+					// check afters
+					for (int a = testInsertIndex; a < order.Count; a++)
+					{
+						int afterIndex = order[a];
+						var cmp = GetOrder(orderFunc, item, items[afterIndex]);
+						switch (cmp)
+						{
+							case DynamicOrder.RequireBBeforeA:
+								invalid = true;
+								break;
+							case DynamicOrder.PreferBBeforeA:
+								score--;
+								break;
+							case DynamicOrder.PreferABeforeB:
+							case DynamicOrder.RequireABeforeB:
+								score++;
+								break;
+							case DynamicOrder.DoesNotMatter:
+								break;
+						}
+						numPrecedingDeps += myDeps;
+					}
+					if (invalid)
+						continue;
+
+					if ((score > bestScore) || (score == bestScore && numPrecedingDeps < bestPrecedingDeps))
+					{
+						bestScore = score;
+						bestPrecedingDeps = numPrecedingDeps;
+						bestInsertionIndex = testInsertIndex;
+					}
+				}
+
+				if (bestInsertionIndex == -1)
+				{
+					CoreException.Throw("Failed to insert " + item + " for dynamic ordering");
+					return null;
+				}
+
+				order.Insert(bestInsertionIndex, insertOriginalIndex);
+
+				//Console.WriteLine("Inserting " + items[insertOriginalIndex] + " at position " + bestInsertionIndex);
+				//for (int i = 0; i < order.Count; i++)
+				//	Console.WriteLine("  " + i + ": " + items[order[i]]);
+			}
+
+			// create ordered array
+			TItem[] result = new TItem[items.Length];
+			for (int i = 0; i < result.Length; i++)
+				result[i] = items[order[i]];
+
+			return result;
+		}
+
+		public static DynamicOrder GetOrder(Func<TItem, TItem, DynamicOrder> order, TItem a, TItem b)
+		{
+			var cmp = order(a, b);
+			var rev = order(b, a);
+
+			return Reconcile(cmp, rev);
+		}
+
+		public static DynamicOrder Reconcile(DynamicOrder AtoB, DynamicOrder BtoA)
+		{
+			switch (AtoB)
+			{
+				case DynamicOrder.DoesNotMatter:
+					return Reverse(BtoA);
+
+				case DynamicOrder.RequireABeforeB:
+					CoreException.Assert(BtoA != DynamicOrder.RequireABeforeB, "Inconsistent dynamic ordering");
+					return AtoB;
+
+				case DynamicOrder.RequireBBeforeA:
+					CoreException.Assert(BtoA != DynamicOrder.RequireBBeforeA, "Inconsistent dynamic ordering");
+					return AtoB;
+
+				case DynamicOrder.PreferABeforeB:
+					if (BtoA == DynamicOrder.PreferABeforeB)
+						return DynamicOrder.DoesNotMatter; // clash in preferrence; but it's ok
+					if (BtoA == DynamicOrder.RequireABeforeB)
+						return DynamicOrder.RequireBBeforeA; // require takes precedence
+					return AtoB;
+
+				case DynamicOrder.PreferBBeforeA:
+					if (BtoA == DynamicOrder.PreferBBeforeA)
+						return DynamicOrder.DoesNotMatter; // clash in preferrence; but it's ok
+					if (BtoA == DynamicOrder.RequireBBeforeA)
+						return DynamicOrder.RequireABeforeB; // require takes precedence
+					return AtoB;
+
+				default:
+					CoreException.ThrowNotImplemented();
+					return DynamicOrder.DoesNotMatter;
+			}
+		}
+
+		private static DynamicOrder Reverse(DynamicOrder cmp)
+		{
+			switch (cmp)
+			{
+				case DynamicOrder.RequireABeforeB:
+					return DynamicOrder.RequireBBeforeA;
+				case DynamicOrder.PreferABeforeB:
+					return DynamicOrder.PreferBBeforeA;
+				case DynamicOrder.DoesNotMatter:
+					return DynamicOrder.DoesNotMatter;
+				case DynamicOrder.PreferBBeforeA:
+					return DynamicOrder.PreferABeforeB;
+				case DynamicOrder.RequireBBeforeA:
+					return DynamicOrder.RequireABeforeB;
+				default:
+					CoreException.ThrowNotImplemented();
+					return DynamicOrder.DoesNotMatter;
+			}
+		}
+	}
+}
