0a7678f8eac5276fcf4090aafa48ecf371acd7ce
Michael Lidgren <lidgren@gmail.com>
2020-09-10 11:28:43 +0200

    MathUtils and Ulid added

---
+++ MathUtils.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Lidgren.Core
+{
+	public static class MathUtils
+	{
+		public const float E = 2.718282f;
+		public const float Log10E = 0.4342945f;
+		public const float Log2E = 1.442695f;
+		public const float Pi = 3.141592653589793239f;
+		public const float HalfPi = 1.570796326794896619f;
+		public const float QuarterPi = 0.785398163397448310f;
+		public const float TwoPi = 6.283185307179586f;
+		public const float InvTwoPi = (float)(1.0 / 6.283185307179586);
+		public const float PiTimesPi = (float)(3.141592653589793239 * 3.141592653589793239);
+
+		public static readonly int[] Primes = {
+			3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
+			1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
+			17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363, 156437,
+			187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
+			1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369 };
+
+		/// <summary>
+		/// Branchless sign; returns 0 if value < 0 and 1 if value >= 0
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int IsPositive(int v)
+		{
+			return (int)(1u ^ ((uint)v >> (int)31)); // if v < 0 then 0, else 1
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int Max(int x, int y)
+		{
+			// return a > b ? a : b;
+			int z = x - y;
+			return x - ((z >> 31) & z);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float Max(float a, float b)
+		{
+			return a > b ? a : b;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float Max(float a, float b, float c)
+		{
+			return a > b ? (a > c ? a : c) : (b > c ? b : c);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double Max(double a, double b)
+		{
+			return a > b ? a : b;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int Min(int a, int b)
+		{
+			return a < b ? a : b;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static uint Min(uint a, uint b)
+		{
+			return a < b ? a : b;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float Min(float a, float b)
+		{
+			return a < b ? a : b;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double Min(double a, double b)
+		{
+			return a < b ? a : b;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool IsPowerOfTwo(int x)
+		{
+			return ((x & (x - 1)) == 0);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int NextPowerOfTwo(int x)
+		{
+			x--;
+			x |= x >> 1;
+			x |= x >> 2;
+			x |= x >> 4;
+			x |= x >> 8;
+			x |= x >> 16;
+			x++;
+			return x;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double Clamp(double value, double min, double max)
+		{
+			return (value > max) ? max : (value < min) ? min : value;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float Clamp(float value, float min, float max)
+		{
+			return (value > max) ? max : (value < min) ? min : value;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int Clamp(int value, int min, int max)
+		{
+			return (value > max) ? max : (value < min) ? min : value;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float SmoothStep(float value1, float value2, float amount)
+		{
+			float num = MathUtils.Clamp(amount, 0f, 1f);
+			return Interpolate(value1, value2, (num * num) * (3f - (2f * num)));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double SmoothStep(double value1, double value2, double amount)
+		{
+			var num = Clamp(amount, 0.0, 1.0);
+			return Interpolate(value1, value2, (num * num) * (3f - (2f * num)));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float SmoothStep(float amount)
+		{
+			float num = MathUtils.Clamp(amount, 0f, 1f);
+			return (num * num) * (3f - (2f * num));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double SmoothStep(double amount)
+		{
+			var num = Clamp(amount, 0.0, 1.0);
+			return (num * num) * (3.0 - (2.0 * num));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double SmootherStep(double amount)
+		{
+			var num = Clamp(amount, 0.0, 1.0);
+			return num * num * num * (num * (num * 6.0 - 15.0) + 10.0);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float Interpolate(float a, float b, float t)
+		{
+			return MathF.FusedMultiplyAdd(a, (1 - t), (b * t));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static byte Interpolate(byte a, byte b, float t)
+		{
+			return (byte)(a * (1 - t) + b * t);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double Interpolate(double a, double b, double t)
+		{
+			return (a * (1 - t) + b * t);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double DegreesToRadians(double degrees)
+		{
+			//return (Math.PI / 180.0) * degrees;
+			return 0.017453292519943295 * degrees;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float DegreesToRadians(float degrees)
+		{
+			//return (float)(Math.PI / 180.0) * degrees;
+			return (float)(0.017453292519943295 * degrees);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float RadiansToDegrees(float radians)
+		{
+			return (float)(57.295779513082320876798154814105 * radians);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double RadiansToDegrees(double radians)
+		{
+			return 57.295779513082320876798154814105 * radians;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int ToNearestMultiple(int n, int multiple)
+		{
+			return ((n + multiple - 1) / multiple) * multiple;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float AngleToSlope(float degrees)
+		{
+			return (float)Math.Tan(MathUtils.DegreesToRadians((float)degrees));
+		}
+
+		/// <summary>
+		/// return x % y but y must be power of two
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int RemainderByPowerOfTwo(int x, int y)
+		{
+			return x & (y - 1);
+		}
+
+		/// <summary>
+		/// return x % y but y must be power of two
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static long RemainderByPowerOfTwo(long x, long y)
+		{
+			return x & (y - 1);
+		}
+
+		/// <summary>
+		/// Euclidean Modulo (not just remainder)
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double Modulo(double value, double mod)
+		{
+			var m = value % mod;
+			if (m < 0)
+				m = (mod < 0) ? m - mod : m + mod;
+			return m;
+		}
+
+		/// <summary>
+		/// Euclidean Modulo (not just remainder)
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float Modulo(float value, float mod)
+		{
+			var m = value % mod;
+			if (m < 0)
+				m = (mod < 0) ? m - mod : m + mod;
+			return m;
+		}
+
+		/// <summary>
+		/// Euclidean Modulo (not just remainder)
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int Modulo(int value, int mod)
+		{
+			var m = value % mod;
+			if (m < 0)
+				m = (mod < 0) ? m - mod : m + mod;
+			return m;
+		}
+
+		/// <summary>
+		/// population standard deviation
+		/// </summary>
+		public static float StandardDeviation(ReadOnlySpan<float> values)
+		{
+			double avg = 0.0;
+			foreach (var val in values)
+				avg += val;
+			avg = avg / (double)values.Length;
+
+			double sqrdAvg = 0.0;
+			foreach (var val in values)
+			{
+				var delta = val - avg;
+				sqrdAvg += (delta * delta);
+			}
+			return MathF.Sqrt((float)sqrdAvg / (float)values.Length);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float FasterSin(float radians)
+		{
+			CoreException.Assert(radians >= -MathUtils.Pi && radians <= MathUtils.Pi);
+			if (radians < 0)
+				return radians * (1.27323954f + 0.405284735f * radians);
+			else
+				return radians * (1.27323954f - 0.405284735f * radians);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float FastSin(float radians)
+		{
+			//var ranged = x - MathUtils.TwoPi * MathF.Floor((x + MathUtils.Pi) / MathUtils.TwoPi);
+			const float negTwoPi = -MathUtils.TwoPi;
+			var pre = MathF.Floor((radians + MathUtils.Pi) * MathUtils.InvTwoPi);
+			var ranged = MathF.FusedMultiplyAdd(negTwoPi, pre, radians);
+
+			var xa = (MathUtils.Pi - ranged) * ranged;
+			var over = 16 * xa;
+			var under = MathF.FusedMultiplyAdd(5, MathUtils.PiTimesPi, (xa * -4));
+			return over / under;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float FastCos(float radians)
+		{
+			// cos(x) = sin(x + PI/2)
+			return FastSin(radians + MathUtils.HalfPi);
+		}
+
+		public static bool IsPrime(int candidate)
+		{
+			if ((candidate & 1) != 0)
+			{
+				int num = (int)Math.Sqrt((double)candidate);
+				for (int i = 3; i <= num; i += 2)
+				{
+					if (candidate % i == 0)
+						return false;
+				}
+				return true;
+			}
+			return candidate == 2;
+		}
+
+		public static int GetPrime(int min)
+		{
+			for (int i = 0; i < Primes.Length; i++)
+			{
+				int num = Primes[i];
+				if (num >= min)
+					return num;
+			}
+
+			for (int j = min | 1; j < int.MaxValue; j += 2)
+			{
+				if (IsPrime(j) && (j - 1) % 101 != 0)
+					return j;
+			}
+			return min;
+		}
+
+		public static int ExpandPrime(int oldSize)
+		{
+			int num = 2 * oldSize;
+			if ((uint)num > 2146435069u && 2146435069 > oldSize)
+			{
+				return 2146435069;
+			}
+			return GetPrime(num);
+		}
+
+		/// <summary>
+		/// Reduce a floating point value; so that its base-10 representation is as few decimals as possible
+		/// </summary>
+		public static float CropMantissa(float value, int bits)
+		{
+			SingleUIntUnion un;
+			un.UIntValue = 0;
+			un.SingleValue = value;
+
+			// reduce by one bit at a time
+			uint signAndExponent = un.UIntValue & 0b11111111_10000000_00000000_00000000u;
+			uint mantissa = un.UIntValue & 0b00000000_01111111_11111111_11111111u;
+
+			uint mask = ~0u << bits;
+			mantissa = mantissa & mask;
+
+			un.UIntValue = signAndExponent | mantissa;
+			return un.SingleValue;
+		}
+	}
+}
---
+++ Ulid.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Lidgren.Core
+{
+	/// <summary>
+	/// Alternative to Guid; see https://github.com/ulid/spec
+	/// </summary>
+	[StructLayout(LayoutKind.Sequential)]
+	public readonly struct Ulid : IEquatable<Ulid>, IComparable<Ulid>
+	{
+		public const int SizeInBytes = 16;
+
+		public static readonly Ulid Empty = new Ulid(0, 0);
+
+		private static readonly long s_timeOffset = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - (long)TimeService.TicksToMilliSeconds(Stopwatch.GetTimestamp());
+
+		public readonly ulong Low;
+		public readonly ulong High;
+
+		private const string k_encode = "0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+		private static readonly byte[] s_decode = new byte[] { 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 255, 255, 255, 255, 255, 255, 255, 10, 11, 12, 13, 14, 15, 16, 17, 255, 18, 19, 255, 20, 21, 255, 22, 23, 24, 25, 26, 255, 27, 28, 29, 30, 31, 255, 255, 255, 255, 255, 255, 10, 11, 12, 13, 14, 15, 16, 17, 255, 18, 19, 255, 20, 21, 255, 22, 23, 24, 25, 26, 255, 27, 28, 29, 30, 31 };
+
+		public Ulid(ulong low, ulong high)
+		{
+			Low = low;
+			High = high;
+		}
+
+		public Ulid(ReadOnlySpan<byte> fromBytes)
+		{
+			CoreException.Assert(fromBytes.Length >= 16);
+			High = BinaryPrimitives.ReadUInt64LittleEndian(fromBytes.Slice(0, 8));
+			Low = BinaryPrimitives.ReadUInt64LittleEndian(fromBytes.Slice(8, 8));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Ulid Create()
+		{
+			var time = s_timeOffset + TimeService.TicksToMilliSeconds(Stopwatch.GetTimestamp());
+			return new Ulid(PRNG.NextUInt64(), ((ulong)time << 16) | (PRNG.NextUInt64() & 0xFFFF));
+		}
+
+		public static Ulid Create(ref ulong rndState)
+		{
+			var time = s_timeOffset + TimeService.TicksToMilliSeconds(Stopwatch.GetTimestamp());
+			return new Ulid(PRNG.NextUInt64(ref rndState), ((ulong)time << 16) | (PRNG.NextUInt64(ref rndState) & 0xFFFF));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Ulid Create(in DateTimeOffset dto)
+		{
+			var time = dto.ToUnixTimeMilliseconds();
+			return new Ulid(PRNG.NextUInt64(), ((ulong)time << 16) | (PRNG.NextUInt64() & 0xFFFF));
+		}
+
+		public static Ulid Create(in DateTimeOffset dto, ref ulong rndState)
+		{
+			var time = dto.ToUnixTimeMilliseconds();
+			return new Ulid(PRNG.NextUInt64(ref rndState), ((ulong)time << 16) | (PRNG.NextUInt64(ref rndState) & 0xFFFF));
+		}
+
+		public static Ulid Parse(ReadOnlySpan<char> fromString)
+		{
+			CoreException.Assert(fromString.Length >= 26);
+#if DEBUG
+			for (int i = 0; i < 26; i++)
+			{
+				var c = fromString[i];
+				if (s_decode[c] == 255)
+					throw new Exception("Bad char in Ulid string: " + c);
+			}
+#endif
+			ulong high =
+				(ulong)s_decode[fromString[0]] << 61 |
+				((ulong)s_decode[fromString[1]] << 56) |
+				((ulong)s_decode[fromString[2]] << 51) |
+				((ulong)s_decode[fromString[3]] << 46) |
+				((ulong)s_decode[fromString[4]] << 41) |
+				((ulong)s_decode[fromString[5]] << 36) |
+				((ulong)s_decode[fromString[6]] << 31) |
+				((ulong)s_decode[fromString[7]] << 26) |
+				((ulong)s_decode[fromString[8]] << 21) |
+				((ulong)s_decode[fromString[9]] << 16) |
+				((ulong)s_decode[fromString[10]] << 11) |
+				((ulong)s_decode[fromString[11]] << 6) |
+				((ulong)s_decode[fromString[12]] << 1);
+
+			ulong straddle = s_decode[fromString[13]];
+			high |= (straddle >> 4); // 1 bit from straddle to high
+
+			ulong low = (ulong)straddle << 60 | // keep 4 bits from straddle
+				((ulong)s_decode[fromString[14]] << 55) |
+				((ulong)s_decode[fromString[15]] << 50) |
+				((ulong)s_decode[fromString[16]] << 45) |
+				((ulong)s_decode[fromString[17]] << 40) |
+				((ulong)s_decode[fromString[18]] << 35) |
+				((ulong)s_decode[fromString[19]] << 30) |
+				((ulong)s_decode[fromString[20]] << 25) |
+				((ulong)s_decode[fromString[21]] << 20) |
+				((ulong)s_decode[fromString[22]] << 15) |
+				((ulong)s_decode[fromString[23]] << 10) |
+				((ulong)s_decode[fromString[24]] << 5) |
+				((ulong)s_decode[fromString[25]]);
+
+			return new Ulid(low, high);
+		}
+
+		public void AsString(Span<char> into)
+		{
+			into[0] = k_encode[(int)(High >> 61)];
+			into[1] = k_encode[(int)((High >> 56) & 0b11111)];
+			into[2] = k_encode[(int)((High >> 51) & 0b11111)];
+			into[3] = k_encode[(int)((High >> 46) & 0b11111)];
+			into[4] = k_encode[(int)((High >> 41) & 0b11111)];
+			into[5] = k_encode[(int)((High >> 36) & 0b11111)];
+			into[6] = k_encode[(int)((High >> 31) & 0b11111)];
+			into[7] = k_encode[(int)((High >> 26) & 0b11111)];
+			into[8] = k_encode[(int)((High >> 21) & 0b11111)];
+			into[9] = k_encode[(int)((High >> 16) & 0b11111)];
+			into[10] = k_encode[(int)((High >> 11) & 0b11111)];
+			into[11] = k_encode[(int)((High >> 6) & 0b11111)];
+			into[12] = k_encode[(int)((High >> 1) & 0b11111)];
+
+			var straddle = ((High & 0b1) << 4) | ((Low >> 60) & 0b1111);
+			into[13] = k_encode[(int)straddle];
+
+			into[14] = k_encode[(int)((Low >> 55) & 0b11111)];
+			into[15] = k_encode[(int)((Low >> 50) & 0b11111)];
+			into[16] = k_encode[(int)((Low >> 45) & 0b11111)];
+			into[17] = k_encode[(int)((Low >> 40) & 0b11111)];
+			into[18] = k_encode[(int)((Low >> 35) & 0b11111)];
+			into[19] = k_encode[(int)((Low >> 30) & 0b11111)];
+			into[20] = k_encode[(int)((Low >> 25) & 0b11111)];
+			into[21] = k_encode[(int)((Low >> 20) & 0b11111)];
+			into[22] = k_encode[(int)((Low >> 15) & 0b11111)];
+			into[23] = k_encode[(int)((Low >> 10) & 0b11111)];
+			into[24] = k_encode[(int)((Low >> 5) & 0b11111)];
+			into[25] = k_encode[(int)(Low & 0b11111)];
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void AsBytes(Span<byte> into)
+		{
+			into[0] = (byte)High;
+			into[1] = (byte)(High >> 8);
+			into[2] = (byte)(High >> 16);
+			into[3] = (byte)(High >> 24);
+			into[4] = (byte)(High >> 32);
+			into[5] = (byte)(High >> 40);
+			into[6] = (byte)(High >> 48);
+			into[7] = (byte)(High >> 56);
+			into[8] = (byte)Low;
+			into[9] = (byte)(Low >> 8);
+			into[10] = (byte)(Low >> 16);
+			into[11] = (byte)(Low >> 24);
+			into[12] = (byte)(Low >> 32);
+			into[13] = (byte)(Low >> 40);
+			into[14] = (byte)(Low >> 48);
+			into[15] = (byte)(Low >> 56);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void GetTime(out DateTimeOffset time)
+		{
+			time = DateTimeOffset.FromUnixTimeMilliseconds((long)(High >> 16));
+		}
+
+		public override string ToString()
+		{
+			return String.Create<Ulid>(26, this, (span, ulid) =>
+			{
+				ulid.AsString(span);
+			});
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public override bool Equals(object obj)
+		{
+			if (obj is null || !(obj is Ulid))
+				return false;
+			var other = (Ulid)obj;
+			return other.Low == Low && other.High == High;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Equals(Ulid other)
+		{
+			return other.Low == Low && other.High == High;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Equals(in Ulid other)
+		{
+			return other.Low == Low && other.High == High;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool operator ==(in Ulid x, in Ulid y)
+		{
+			return (x.Low == y.Low && x.High == y.High);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool operator !=(in Ulid x, in Ulid y)
+		{
+			return (x.Low != y.Low || x.High != y.High);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public override int GetHashCode()
+		{
+			// Low is all random bits; can be used verbatim
+			// But the time part of High needs to be mixed a bit
+			unchecked
+			{
+				// simple mixing since we're xoring with random bits anyway
+				ulong mixed = High;
+				mixed ^= mixed >> 47;
+				mixed *= 0xc6a4a7935bd1e995ul;
+				mixed ^= mixed >> 47;
+
+				ulong hash = mixed ^ Low;
+
+				// xor fold to 32 bits
+				return (int)((uint)hash ^ (uint)(hash >> 32));
+			}
+		}
+
+		public int CompareTo(Ulid other)
+		{
+			if (High < other.High)
+				return -1;
+			if (High > other.High)
+				return 1;
+			if (Low < other.Low)
+				return -1;
+			if (Low > other.Low)
+				return 1;
+			return 0;
+		}
+	}
+}
