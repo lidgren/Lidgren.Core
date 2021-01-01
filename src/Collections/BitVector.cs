2e2d81f6e04ef27ee3b72206453bfd61f9f4fd41
Michael Lidgren <lidgren@gmail.com>
2021-01-01 17:14:36 +0100

    BitVector and tests

---
+++ src/Collections/BitVector.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+
+namespace Lidgren.Core
+{
+	/// <summary>
+	/// Arbitrarily long bit vector
+	/// </summary>
+	public sealed class BitVector
+	{
+		private const int c_bitsPerUnit = 64;
+		private const int c_bitsMask = 63;
+		private const int c_unitBitCount = 6;
+
+		private ulong[] m_data;
+		public ulong[] Data => m_data;
+
+		public BitVector(int minCapacity = c_bitsPerUnit)
+		{
+			int numUnits = UnitsToHoldBits(minCapacity);
+			m_data = new ulong[numUnits];
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private int UnitsToHoldBits(int bits)
+		{
+			return ((bits - 1) / c_bitsPerUnit) + 1;
+		}
+
+		public bool this[int index]
+		{
+			get { return Get(index); }
+			set { Set(index, value); }
+		}
+
+		/// <summary>
+		/// Clear all values to false
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Clear()
+		{
+			m_data.AsSpan().Clear();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void SetAll(bool value)
+		{
+			if (value)
+				m_data.AsSpan().Fill(~0ul);
+			else
+				m_data.AsSpan().Clear();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Get(int index)
+		{
+			int dataIndex = index >> c_unitBitCount;
+			ulong value = m_data[dataIndex];
+			int bitIndex = index & c_bitsMask;
+			return ((value >> bitIndex) & 1ul) != 0ul;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Flip(int index)
+		{
+			int dataIndex = index >> c_unitBitCount;
+			int bitIndex = index & c_bitsMask;
+			ulong mask = 1ul << bitIndex;
+			m_data[dataIndex] ^= mask;
+		}
+
+		/// <summary>
+		/// Set index to true
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Set(int index)
+		{
+			int dataIndex = index >> c_unitBitCount;
+			int bitIndex = index & c_bitsMask;
+			ulong mask = 1ul << bitIndex;
+			m_data[dataIndex] |= mask;
+		}
+
+		/// <summary>
+		/// Set index to false
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Clear(int index)
+		{
+			int dataIndex = index >> c_unitBitCount;
+			int bitIndex = index & c_bitsMask;
+			ulong mask = 1ul << bitIndex;
+			m_data[dataIndex] &= ~mask;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void Set(int index, bool value)
+		{
+			int dataIndex = index >> c_unitBitCount;
+			int bitIndex = index & c_bitsMask;
+			ulong mask = 1ul << bitIndex;
+			if (value)
+				m_data[dataIndex] |= mask;
+			else
+				m_data[dataIndex] &= ~mask;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public int CountSetBits()
+		{
+			int retval = 0;
+			for (int i = 0; i < m_data.Length; i++)
+				retval += BitOperations.PopCount(m_data[i]);
+			return retval;
+		}
+	}
+}
---
+++ tests/BitVectorTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Lidgren.Core;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class BitVectorTests
+	{
+		[TestMethod]
+		public void TestBitVector()
+		{
+			var facit = new bool[9 * 64];
+			for (int r = 1; r < 8; r++)
+			{
+				var sz = r * 64;
+				var bv = new BitVector(sz);
+				facit.AsSpan().Clear();
+
+				for (int op = 0; op < 10000; op++)
+				{
+					var a = PRNG.Next(0, sz);
+					var t = PRNG.Next(0, 14);
+					switch (t)
+					{
+						case 0:
+							bv.Clear();
+							facit.AsSpan().Clear();
+							break;
+						case 1:
+							bv.Set(a, true);
+							facit[a] = true;
+							break;
+						case 2:
+						case 3:
+							bv.Set(a);
+							facit[a] = true;
+							break;
+						case 4:
+						case 5:
+						case 6:
+							bv[a] = true;
+							facit[a] = true;
+							break;
+						case 7:
+						case 8:
+							bv.Clear(a);
+							facit[a] = false;
+							break;
+						case 9:
+							bv.Set(a, false);
+							facit[a] = false;
+							break;
+						case 10:
+							bv[a] = false;
+							facit[a] = false;
+							break;
+						case 11:
+						case 12:
+						case 13:
+							bv.Flip(a);
+							if (facit[a])
+								facit[a] = false;
+							else
+								facit[a] = true;
+							break;
+					}
+
+					// verify
+					var cnt = 0;
+					for (int i = 0; i < sz; i++)
+					{
+						Assert.AreEqual(facit[i], bv[i]);
+						if (facit[i])
+							cnt++;
+					}
+					Assert.AreEqual(cnt, bv.CountSetBits());
+				}
+			}
+		}
+	}
+}
