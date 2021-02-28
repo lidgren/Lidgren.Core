6d5c04aa4081a5e8e59099b6c193639deca3bc46
Michael Lidgren <lidgren@gmail.com>
2021-03-01 00:46:10 +0100

    ChromeTraceTimingConsumer now uses FixedStringBuilder

--- src/Timing/ChromeTraceTimingConsumer.cs
+++ src/Timing/ChromeTraceTimingConsumer.cs
@@ -15,7 +15,8 @@ namespace Lidgren.Core
 
 		public ChromeTraceTimingConsumer(string filename)
 		{
-			m_invFrequencyToMicros = 1000000.0 / (double)Stopwatch.Frequency;
+			//m_invFrequencyToMicros = 1000000.0 / (double)Stopwatch.Frequency;
+			m_invFrequencyToMicros = 1000000000.0 / (double)Stopwatch.Frequency;
 
 			var fs = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.ReadWrite | FileShare.Delete);
 			m_writer = new StreamWriter(fs);
@@ -24,14 +25,15 @@ namespace Lidgren.Core
 			TimingService.AddListener(Flush);
 		}
 
-		private const string k_part0 = ",{\"name\":\"";
-		private const string k_part1 = "\",\"ph\":\"X\",\"ts\":";
-		private const string k_part2 = ",\"dur\":";
-		private const string k_part3 = ",\"pid\":0,\"tid\":";
+		private const string k_namePart = ",{\"name\":\"";
 
 		private void Flush(TimingThread thread, TimingEntry[] entries, int count)
 		{
 			var span = entries.AsSpan(0, count);
+
+			var writer = m_writer;
+			if (writer == null)
+				return;
 
 			lock (m_seen)
 			{
@@ -39,69 +41,46 @@ namespace Lidgren.Core
 				{
 					// output thread name to chrome trace
 					// we can affort to do this inefficiently since it's only done once per thread
-					m_writer?.WriteLine(", { \"name\": \"thread_name\", \"ph\": \"M\", \"pid\": 0, \"tid\": " + thread.Index.ToString() + ", \"args\": { \"name\": \"" + thread.Name + "\" } }");
+					writer.WriteLine(", { \"name\": \"thread_name\", \"ph\": \"M\", \"pid\": 0, \"tid\": " + thread.Index.ToString() + ", \"args\": { \"name\": \"" + thread.Name + "\" } }");
 					m_seen.Add(thread);
 				}
 			}
 
-			// some acrobatics here to avoid string allocations
-
-			// assume thread name < 64 chars
-			Span<char> buffer = stackalloc char[64 + 5 + k_part0.Length + k_part1.Length + k_part2.Length];
-			k_part0.AsSpan().CopyTo(buffer);
-			var work = buffer.Slice(k_part0.Length);
-
 			var invFrequencyToMicros = m_invFrequencyToMicros;
 			var ti = thread.Index;
 
+			Span<char> buf = stackalloc char[256];
+			var bdr = new FixedStringBuilder(buf);
+			bdr.Append(k_namePart);
 			for (int i = 0; i < span.Length; i++)
 			{
+				bdr.Length = k_namePart.Length; // keep name part only
+
 				ref readonly var entry = ref span[i];
 
-				// part0 is already added
-				var rem = work;
-
-				// add name
-				var name = entry.Name.AsSpan();
-				name.CopyTo(rem);
-				rem = rem.Slice(name.Length);
-
-				// add part1
-				k_part1.AsSpan().CopyTo(rem);
-				rem = rem.Slice(k_part1.Length);
+				bdr.Append(entry.Name);
 
 				// add start time
+				bdr.Append("\",\"ph\":\"X\",\"ts\":");
 				ulong startMicros = (ulong)((double)entry.Start * invFrequencyToMicros);
-				bool ok = startMicros.TryFormat(rem, out int written);
-				CoreException.Assert(ok);
-				rem = rem.Slice(written);
-
-				// add part2
-				k_part2.AsSpan().CopyTo(rem);
-				rem = rem.Slice(k_part2.Length);
+				bdr.Append(startMicros);
 
 				// add duration
+				bdr.Append(",\"dur\":");
 				ulong durMicros = (ulong)((double)entry.Duration * invFrequencyToMicros);
-				ok = durMicros.TryFormat(rem, out written);
-				CoreException.Assert(ok);
-				rem = rem.Slice(written);
-
-				// add part3
-				k_part3.AsSpan().CopyTo(rem);
-				rem = rem.Slice(k_part3.Length);
+				if (durMicros <= 0)
+					durMicros = 1;
+				bdr.Append(durMicros);
 
 				// add thread index
-				ok = ti.TryFormat(rem, out written);
-				CoreException.Assert(ok);
-				rem = rem.Slice(written);
+				bdr.Append(",\"pid\":0,\"tid\":");
+				bdr.Append(ti);
 
 				// add end
-				rem[0] = '}';
-				rem = rem.Slice(1);
+				bdr.Append('}');
 
 				// write line to stream
-				int lineLen = buffer.Length - rem.Length;
-				m_writer?.WriteLine(buffer.Slice(0, lineLen));
+				writer.WriteLine(bdr.ReadOnlySpan);
 			}
 		}
 
