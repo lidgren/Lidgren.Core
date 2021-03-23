bbc5a8705c185b40cacdc76b7fbd4a58df6ac5db
Michael Lidgren <lidgren@gmail.com>
2021-03-23 11:50:44 +0100

    Added more PriorityQueue tests

--- tests/PriorityQueueTests.cs
+++ tests/PriorityQueueTests.cs
@@ -11,110 +11,139 @@ namespace UnitTests
 		[TestMethod]
 		public void TestPriorityQueue()
 		{
-			var queue = new PriorityQueue<int, string>(4);
-
-			queue.Enqueue(5, "five");
-			queue.Enqueue(3, "three");
-			queue.Enqueue(8, "eight");
-			queue.Enqueue(1, "one");
-			queue.Enqueue(3, "three-again");
-
-			bool ok = queue.TryDequeue(out var str);
-			Assert.IsTrue(ok);
-			Assert.AreEqual("one", str);
-
-			ok = queue.TryDequeue(out str);
-			Assert.IsTrue(ok);
-			Assert.IsTrue(str == "three" || str == "three-again");
-			ok = queue.TryDequeue(out str);
-			Assert.IsTrue(ok);
-			Assert.IsTrue(str == "three" || str == "three-again");
-
-			ok = queue.PeekPriority(out var nextPrio);
-			Assert.IsTrue(ok);
-			Assert.AreEqual(5, nextPrio);
-
-			ok = queue.TryDequeue(out str);
-			Assert.IsTrue(ok);
-			Assert.AreEqual("five", str);
-
-			Assert.AreEqual(1, queue.Count);
-
-			queue.Enqueue(6, "six");
-
-			ok = queue.TryDequeue(out str);
-			Assert.IsTrue(ok);
-			Assert.AreEqual("six", str);
-
-			ok = queue.TryDequeue(out str);
-			Assert.IsTrue(ok);
-			Assert.AreEqual("eight", str);
-
-			ok = queue.TryDequeue(out str);
-			Assert.IsFalse(ok);
-			Assert.AreEqual(0, queue.Count);
-
-			queue.Clear();
-			queue.Enqueue(3, "3");
-			queue.Enqueue(1, "1");
-			queue.Enqueue(2, "2");
-			ok = queue.Remove("22");
-			Assert.IsFalse(ok);
-			Assert.AreEqual(3, queue.Count);
-			ok = queue.Remove("2");
-			Assert.IsTrue(ok);
-
-			Assert.AreEqual("1", queue.Dequeue());
-			Assert.AreEqual("3", queue.Dequeue());
-			ok = queue.TryDequeue(out _);
-			Assert.IsFalse(ok);
-
-			var facit = new List<(int, string)>();
-			queue.Clear();
-			for(int op = 0; op < 1000000; op++)
 			{
-				switch (PRNG.Next(0, 8))
+				// just enqueue a bunch
+				var pq = new PriorityQueue<uint, uint>(100);
+				for (int i = 0; i < 1000; i++)
 				{
-					case 0:
-						queue.Clear();
-						facit.Clear();
-						break;
-					case 1:
-					case 2:
-					case 3:
-					case 4:
-						// enqueue
-						int add = PRNG.Next(0, 50);
-						queue.Enqueue(add, add.ToString());
-						facit.Add((add, add.ToString()));
-						break;
-					case 5:
-						// remove
-						int rem = PRNG.Next(0, 50);
-						var fok = Remove(facit, rem.ToString());
-						var qok = queue.Remove(rem.ToString());
-						Assert.AreEqual(fok, qok);
-						break;
-					case 6:
-					case 7:
-						// dequeue
-						if (facit.Count <= 0)
-							continue;
-						var fitem = Dequeue(facit);
-						var qitem = queue.Dequeue();
-						Assert.AreEqual(fitem, qitem);
-						break;
+					var r = PRNG.NextUInt32();
+					if (r == 0)
+						r = 1;
+					pq.Enqueue(r, r);
 				}
 
-				Assert.AreEqual(facit.Count, queue.Count);
-				if (facit.Count > 0)
+				// dequeue and verify
+				uint last = 0;
+				while (pq.Count > 0)
 				{
-					ok = queue.Peek(out var peekprio, out var peekitem);
+					var ok = pq.Peek(out var nextprio, out var nextItem);
 					Assert.IsTrue(ok);
-					Peek(facit, out var facitpeekprio, out var facitpeekitem);
+					Assert.AreEqual(nextprio, nextItem);
+					Assert.IsTrue(nextprio >= last);
+					last = nextprio;
 
-					Assert.AreEqual(facitpeekprio, peekprio);
-					Assert.AreEqual(facitpeekitem, peekitem);
+					var pok = pq.TryDequeue(out var dequeuedItem);
+					Assert.IsTrue(pok);
+					Assert.AreEqual(dequeuedItem, nextItem);
+				}
+			}
+
+			{
+				var queue = new PriorityQueue<int, string>(4);
+
+				queue.Enqueue(5, "five");
+				queue.Enqueue(3, "three");
+				queue.Enqueue(8, "eight");
+				queue.Enqueue(1, "one");
+				queue.Enqueue(3, "three-again");
+
+				bool ok = queue.TryDequeue(out var str);
+				Assert.IsTrue(ok);
+				Assert.AreEqual("one", str);
+
+				ok = queue.TryDequeue(out str);
+				Assert.IsTrue(ok);
+				Assert.IsTrue(str == "three" || str == "three-again");
+				ok = queue.TryDequeue(out str);
+				Assert.IsTrue(ok);
+				Assert.IsTrue(str == "three" || str == "three-again");
+
+				ok = queue.PeekPriority(out var nextPrio);
+				Assert.IsTrue(ok);
+				Assert.AreEqual(5, nextPrio);
+
+				ok = queue.TryDequeue(out str);
+				Assert.IsTrue(ok);
+				Assert.AreEqual("five", str);
+
+				Assert.AreEqual(1, queue.Count);
+
+				queue.Enqueue(6, "six");
+
+				ok = queue.TryDequeue(out str);
+				Assert.IsTrue(ok);
+				Assert.AreEqual("six", str);
+
+				ok = queue.TryDequeue(out str);
+				Assert.IsTrue(ok);
+				Assert.AreEqual("eight", str);
+
+				ok = queue.TryDequeue(out str);
+				Assert.IsFalse(ok);
+				Assert.AreEqual(0, queue.Count);
+
+				queue.Clear();
+				queue.Enqueue(3, "3");
+				queue.Enqueue(1, "1");
+				queue.Enqueue(2, "2");
+				ok = queue.Remove("22");
+				Assert.IsFalse(ok);
+				Assert.AreEqual(3, queue.Count);
+				ok = queue.Remove("2");
+				Assert.IsTrue(ok);
+
+				Assert.AreEqual("1", queue.Dequeue());
+				Assert.AreEqual("3", queue.Dequeue());
+				ok = queue.TryDequeue(out _);
+				Assert.IsFalse(ok);
+
+				var facit = new List<(int, string)>();
+				queue.Clear();
+				for (int op = 0; op < 1000000; op++)
+				{
+					switch (PRNG.Next(0, 8))
+					{
+						case 0:
+							queue.Clear();
+							facit.Clear();
+							break;
+						case 1:
+						case 2:
+						case 3:
+						case 4:
+							// enqueue
+							int add = PRNG.Next(0, 50);
+							queue.Enqueue(add, add.ToString());
+							facit.Add((add, add.ToString()));
+							break;
+						case 5:
+							// remove
+							int rem = PRNG.Next(0, 50);
+							var fok = Remove(facit, rem.ToString());
+							var qok = queue.Remove(rem.ToString());
+							Assert.AreEqual(fok, qok);
+							break;
+						case 6:
+						case 7:
+							// dequeue
+							if (facit.Count <= 0)
+								continue;
+							var fitem = Dequeue(facit);
+							var qitem = queue.Dequeue();
+							Assert.AreEqual(fitem, qitem);
+							break;
+					}
+
+					Assert.AreEqual(facit.Count, queue.Count);
+					if (facit.Count > 0)
+					{
+						ok = queue.Peek(out var peekprio, out var peekitem);
+						Assert.IsTrue(ok);
+						Peek(facit, out var facitpeekprio, out var facitpeekitem);
+
+						Assert.AreEqual(facitpeekprio, peekprio);
+						Assert.AreEqual(facitpeekitem, peekitem);
+					}
 				}
 			}
 		}
