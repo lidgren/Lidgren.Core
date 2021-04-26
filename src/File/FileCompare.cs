1061909aabdcc299bbe6af2cd04ddc885999a54b
Michael Lidgren <lidgren@gmail.com>
2021-04-26 19:54:58 +0200

    FileCompare updated and unit tests added

--- src/File/FileCompare.cs
+++ src/File/FileCompare.cs
@@ -7,63 +7,36 @@ namespace Lidgren.Core
 {
 	public static class FileCompare
 	{
-		/// <summary>
-		/// Returns true if the contents of the files are binary equal
-		/// </summary>
-		public static bool AreEqual(string filename1, string filename2)
-		{
-			return AreEqual(filename1, filename2, 1024 * 32);
-		}
+		private const int kDefaultBufferSize = 1024 * 16;
 
 		/// <summary>
-		/// Returns true if the contents of the files are binary equal
+		/// Compare content of two files; buffers must be equal length and multiple of 8
 		/// </summary>
-		public static bool AreEqual(string filename1, string filename2, int bufSize)
+		public static bool AreEqual(string filename1, string filename2, int bufSize = kDefaultBufferSize)
 		{
-			CoreException.Assert((bufSize & 7) == 0);
-			var pool = ArrayPool<byte>.Shared;
-
-			byte[] buf1 = null;
-			byte[] buf2 = null;
-			try
+			bool retval = CompareByFileName(filename1, filename2, out bool gotResult, out FileInfo f1, out FileInfo f2);
+			if (gotResult)
+				return retval;
+			using (var fs1 = f1.OpenRead())
 			{
-				buf1 = pool.Rent(bufSize);
-				buf2 = pool.Rent(bufSize);
-				return AreEqual(filename1, filename2, buf1, buf2);
-			}
-			finally
-			{
-				if (buf1 != null)
-					pool.Return(buf1);
-				if (buf2 != null)
-					pool.Return(buf2);
+				using (var fs2 = f2.OpenRead())
+				{
+					return AreEqual(fs1, fs2, bufSize);
+				}
 			}
 		}
 
 		/// <summary>
-		/// Returns true if the contents of the files are binary equal
+		/// Compare content of two files; buffers must be equal length and multiple of 8
 		/// </summary>
-		public static bool AreEqual(string filename1, string filename2, byte[] buf1, byte[] buf2)
+		public static bool AreEqual(string filename1, string filename2, Span<byte> buf1, Span<byte> buf2)
 		{
-			CoreException.Assert(buf1.Length == buf2.Length);
-			CoreException.Assert((buf1.Length & 7) == 0);
-
-			if (filename1.Equals(filename2, StringComparison.OrdinalIgnoreCase))
-				return true; // same file
-
-			// check file sizes
-			var f1 = new FileInfo(filename1);
-			var f2 = new FileInfo(filename2);
-
-			if (f1.Exists == false || f2.Exists == false)
-				return false; // like NaN, if any of them don't exists - there's no equality
-
-			if (f1.Length != f2.Length)
-				return false; // not same size
-
-			using (var fs1 = new FileStream(filename1, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			bool retval = CompareByFileName(filename1, filename2, out bool gotResult, out FileInfo f1, out FileInfo f2);
+			if (gotResult)
+				return retval;
+			using (var fs1 = f1.OpenRead())
 			{
-				using (var fs2 = new FileStream(filename2, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+				using (var fs2 = f2.OpenRead())
 				{
 					return AreEqual(fs1, fs2, buf1, buf2);
 				}
@@ -71,46 +44,95 @@ namespace Lidgren.Core
 		}
 
 		/// <summary>
-		/// Returns true if the contents of the files are binary equal
+		/// Compare content of two streams; bufSize must be multiple of 8
 		/// </summary>
-		public static bool AreEqual(Stream fs1, Stream fs2, byte[] buf1, byte[] buf2)
+		public static bool AreEqual(Stream fs1, Stream fs2, int bufSize = kDefaultBufferSize)
+		{
+			var pool = ArrayPool<byte>.Shared;
+			byte[] buffer = null;
+			try
+			{
+				buffer = pool.Rent(bufSize * 2);
+				return AreEqual(fs1, fs2, buffer.AsSpan(0, bufSize), buffer.AsSpan(bufSize));
+			}
+			finally
+			{
+				if (buffer != null)
+					pool.Return(buffer);
+			}
+		}
+
+		/// <summary>
+		/// Compare content of two streams; buffers must be equal length and multiple of 8
+		/// </summary>
+		public static bool AreEqual(Stream fs1, Stream fs2, Span<byte> buf1, Span<byte> buf2)
 		{
 			CoreException.Assert(buf1.Length == buf2.Length);
 			CoreException.Assert((buf1.Length & 7) == 0);
 
-			using (var bfs1 = new BufferedStream(fs1))
+			ReadOnlySpan<ulong> ulBuf1 = MemoryMarshal.Cast<byte, ulong>(buf1);
+			ReadOnlySpan<ulong> ulBuf2 = MemoryMarshal.Cast<byte, ulong>(buf2);
+
+			for (; ; )
 			{
-				using (var bfs2 = new BufferedStream(fs2))
+				int len = fs1.Read(buf1);
+				if (len < 1)
+					return true;
+
+				int len2 = fs2.Read(buf2.Slice(0, len));
+
+				if (len != len2)
+					CoreException.Throw("Failed to read part of file");
+
+				if (len == buf1.Length)
 				{
-					ReadOnlySpan<ulong> ulBuf1 = MemoryMarshal.Cast<byte, ulong>(buf1);
-					ReadOnlySpan<ulong> ulBuf2 = MemoryMarshal.Cast<byte, ulong>(buf2);
-
-					while (true)
-					{
-						int len = fs1.Read(buf1, 0, buf1.Length);
-						if (len < 1)
-							return true;
-
-						int len2 = fs2.Read(buf2, 0, len);
-
-						if (len != len2)
-							CoreException.Throw("Failed to read part of file");
-
-						if (len == buf1.Length)
-						{
-							// compare ulongs
-							if (ulBuf1.SequenceEqual(ulBuf2) == false)
-								return false;
-						}
-						else
-						{
-							// compare bytes
-							if (buf1.AsSpan(0, len).SequenceEqual(buf2.AsSpan(0, len)) == false)
-								return false;
-						}
-					}
+					// compare ulongs
+					if (ulBuf1.SequenceEqual(ulBuf2) == false)
+						return false;
+				}
+				else
+				{
+					// compare bytes
+					if (buf1.Slice(0, len).SequenceEqual(buf2.Slice(0, len)) == false)
+						return false;
 				}
 			}
 		}
+
+		private static bool CompareByFileName(string filename1, string filename2, out bool gotResult, out FileInfo f1, out FileInfo f2)
+		{
+			if (string.IsNullOrEmpty(filename1) || string.IsNullOrEmpty(filename2))
+			{
+				f1 = f2 = null;
+				gotResult = true;
+				return false; // like NaN, if any of them don't exists - there's no equality
+			}
+
+			// check file sizes
+			f1 = new FileInfo(filename1);
+			f2 = new FileInfo(filename2);
+
+			if (f1.Exists == false || f2.Exists == false)
+			{
+				gotResult = true;
+				return false; // like NaN, if any of them don't exists - there's no equality
+			}
+
+			if (f1.FullName.Equals(f2.FullName, StringComparison.Ordinal))
+			{
+				gotResult = true;
+				return true;
+			}
+
+			if (f1.Length != f2.Length)
+			{
+				gotResult = true;
+				return false; // not same size
+			}
+
+			gotResult = false;
+			return false;
+		}
 	}
 }
+
---
+++ tests/FileCompareTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Lidgren.Core;
+using System.Reflection;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class FileCompareTests
+	{
+		[TestMethod]
+		public void TextFileCompare()
+		{
+			// get this assembly file
+			var assemblyFile = Assembly.GetExecutingAssembly().Location;
+
+			Assert.IsTrue(FileCompare.AreEqual(assemblyFile, assemblyFile));
+			Assert.IsTrue(FileCompare.AreEqual("FileOne.txt", "FileOne.txt"));
+			Assert.IsTrue(FileCompare.AreEqual("FileOne.txt", "FileTwo.txt"));
+			Assert.IsFalse(FileCompare.AreEqual("FileOne.txt", assemblyFile));
+			Assert.IsFalse(FileCompare.AreEqual("FileOne.txt", "xxx"));
+			Assert.IsFalse(FileCompare.AreEqual("xxx", "FileOne.txt"));
+			Assert.IsFalse(FileCompare.AreEqual("xxx", "yyy"));
+			Assert.IsFalse(FileCompare.AreEqual("xxx", "xxx"));
+			Assert.IsFalse(FileCompare.AreEqual(null, "xxx"));
+
+			int bufSize = 4096;
+			Assert.IsTrue(FileCompare.AreEqual(assemblyFile, assemblyFile, bufSize));
+			Assert.IsTrue(FileCompare.AreEqual("FileOne.txt", "FileOne.txt", bufSize));
+			Assert.IsTrue(FileCompare.AreEqual("FileOne.txt", "FileTwo.txt", bufSize));
+			Assert.IsFalse(FileCompare.AreEqual("FileOne.txt", assemblyFile, bufSize));
+			Assert.IsFalse(FileCompare.AreEqual("FileOne.txt", "xxx", bufSize));
+			Assert.IsFalse(FileCompare.AreEqual("xxx", "FileOne.txt", bufSize));
+			Assert.IsFalse(FileCompare.AreEqual("xxx", "yyy", bufSize));
+			Assert.IsFalse(FileCompare.AreEqual("xxx", "xxx", bufSize));
+			Assert.IsFalse(FileCompare.AreEqual(null, "xxx", bufSize));
+
+			var buffer = new byte[8192];
+			var b1 = buffer.AsSpan(0, 4096);
+			var b2 = buffer.AsSpan(4096);
+			Assert.IsTrue(FileCompare.AreEqual(assemblyFile, assemblyFile, b1, b2));
+			Assert.IsTrue(FileCompare.AreEqual("FileOne.txt", "FileOne.txt", b1, b2));
+			Assert.IsTrue(FileCompare.AreEqual("FileOne.txt", "FileTwo.txt", b1, b2));
+			Assert.IsFalse(FileCompare.AreEqual("FileOne.txt", assemblyFile, b1, b2));
+			Assert.IsFalse(FileCompare.AreEqual("FileOne.txt", "xxx", b1, b2));
+			Assert.IsFalse(FileCompare.AreEqual("xxx", "FileOne.txt", b1, b2));
+			Assert.IsFalse(FileCompare.AreEqual("xxx", "yyy", b1, b2));
+			Assert.IsFalse(FileCompare.AreEqual("xxx", "xxx", b1, b2));
+			Assert.IsFalse(FileCompare.AreEqual(null, "xxx", b1, b2));
+		}
+	}
+}
