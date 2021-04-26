7829bb37cf766f2bf0b1bcb8781bb2cd4035dca0
Michael Lidgren <lidgren@gmail.com>
2021-04-26 21:53:37 +0200

    FileCompare output made more detailed + updated unit tests

--- src/File/FileCompare.cs
+++ src/File/FileCompare.cs
@@ -7,14 +7,47 @@ namespace Lidgren.Core
 {
 	public static class FileCompare
 	{
+		public enum Result
+		{
+			/// <summary>
+			/// The files have identical contents
+			/// </summary>
+			Identical,
+
+			/// <summary>
+			/// The same file was passed in both arguments
+			/// </summary>
+			SameFile,
+
+			/// <summary>
+			/// The files have different contents
+			/// </summary>
+			Different,
+
+			/// <summary>
+			/// File1 does not exist
+			/// </summary>
+			File1Missing,
+
+			/// <summary>
+			/// File2 does not exist
+			/// </summary>
+			File2Missing,
+
+			/// <summary>
+			/// Neither File1 or File2 exists
+			/// </summary>
+			BothFilesMissing,
+		}
+
 		private const int kDefaultBufferSize = 1024 * 16;
 
 		/// <summary>
 		/// Compare content of two files; buffers must be equal length and multiple of 8
 		/// </summary>
-		public static bool AreEqual(string filename1, string filename2, int bufSize = kDefaultBufferSize)
+		public static Result AreEqual(string filename1, string filename2, int bufSize = kDefaultBufferSize)
 		{
-			bool retval = CompareByFileName(filename1, filename2, out bool gotResult, out FileInfo f1, out FileInfo f2);
+			var retval = CompareByFileName(filename1, filename2, out bool gotResult, out FileInfo f1, out FileInfo f2);
 			if (gotResult)
 				return retval;
 			using (var fs1 = f1.OpenRead())
@@ -29,9 +62,9 @@ namespace Lidgren.Core
 		/// <summary>
 		/// Compare content of two files; buffers must be equal length and multiple of 8
 		/// </summary>
-		public static bool AreEqual(string filename1, string filename2, Span<byte> buf1, Span<byte> buf2)
+		public static Result AreEqual(string filename1, string filename2, Span<byte> buf1, Span<byte> buf2)
 		{
-			bool retval = CompareByFileName(filename1, filename2, out bool gotResult, out FileInfo f1, out FileInfo f2);
+			var retval = CompareByFileName(filename1, filename2, out bool gotResult, out FileInfo f1, out FileInfo f2);
 			if (gotResult)
 				return retval;
 			using (var fs1 = f1.OpenRead())
@@ -46,7 +79,7 @@ namespace Lidgren.Core
 		/// <summary>
 		/// Compare content of two streams; bufSize must be multiple of 8
 		/// </summary>
-		public static bool AreEqual(Stream fs1, Stream fs2, int bufSize = kDefaultBufferSize)
+		public static Result AreEqual(Stream fs1, Stream fs2, int bufSize = kDefaultBufferSize)
 		{
 			var pool = ArrayPool<byte>.Shared;
 			byte[] buffer = null;
@@ -65,7 +98,7 @@ namespace Lidgren.Core
 		/// <summary>
 		/// Compare content of two streams; buffers must be equal length and multiple of 8
 		/// </summary>
-		public static bool AreEqual(Stream fs1, Stream fs2, Span<byte> buf1, Span<byte> buf2)
+		public static Result AreEqual(Stream fs1, Stream fs2, Span<byte> buf1, Span<byte> buf2)
 		{
 			CoreException.Assert(buf1.Length == buf2.Length);
 			CoreException.Assert((buf1.Length & 7) == 0);
@@ -77,7 +110,7 @@ namespace Lidgren.Core
 			{
 				int len = fs1.Read(buf1);
 				if (len < 1)
-					return true;
+					return Result.Identical;
 
 				int len2 = fs2.Read(buf2.Slice(0, len));
 
@@ -88,50 +121,57 @@ namespace Lidgren.Core
 				{
 					// compare ulongs
 					if (ulBuf1.SequenceEqual(ulBuf2) == false)
-						return false;
+						return Result.Different;
 				}
 				else
 				{
 					// compare bytes
 					if (buf1.Slice(0, len).SequenceEqual(buf2.Slice(0, len)) == false)
-						return false;
+						return Result.Different;
 				}
 			}
 		}
 
-		private static bool CompareByFileName(string filename1, string filename2, out bool gotResult, out FileInfo f1, out FileInfo f2)
+		private static Result CompareByFileName(string filename1, string filename2, out bool gotResult, out FileInfo f1, out FileInfo f2)
 		{
-			if (string.IsNullOrEmpty(filename1) || string.IsNullOrEmpty(filename2))
+			bool m1 = string.IsNullOrEmpty(filename1);
+			bool m2 = string.IsNullOrEmpty(filename2);
+			if (m1)
 			{
 				f1 = f2 = null;
 				gotResult = true;
-				return false; // like NaN, if any of them don't exists - there's no equality
+				return m2 ? Result.BothFilesMissing : Result.File1Missing;
+			}
+			if (m2)
+			{
+				f1 = f2 = null;
+				gotResult = true;
+				return Result.File2Missing;
 			}
 
-			// check file sizes
+			// check existence and file sizes
 			f1 = new FileInfo(filename1);
 			f2 = new FileInfo(filename2);
 
-			if (f1.Exists == false || f2.Exists == false)
+			if (f1.Exists == false)
 			{
 				gotResult = true;
-				return false; // like NaN, if any of them don't exists - there's no equality
+				return (f2.Exists == false) ? Result.BothFilesMissing : Result.File1Missing;
+			}
+			if (f2.Exists == false)
+			{
+				gotResult = true;
+				return Result.File2Missing;
 			}
 
 			if (f1.FullName.Equals(f2.FullName, StringComparison.Ordinal))
 			{
 				gotResult = true;
-				return true;
+				return Result.SameFile;
 			}
 
-			if (f1.Length != f2.Length)
-			{
-				gotResult = true;
-				return false; // not same size
-			}
-
-			gotResult = false;
-			return false;
+			gotResult = f1.Length != f2.Length;
+			return Result.Different;
 		}
 	}
 }
