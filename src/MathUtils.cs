10c83e6df62c0b9092a96d66ae32708e9843f521
lidgren <lidgren@gmail.com>
2025-02-08 19:44:26 +0100

    Fixed modulo for negative numbers; now adhers to "floored division" modulo

--- src/MathUtils.cs
+++ src/MathUtils.cs
@@ -16,11 +16,11 @@ namespace Lidgren.Core
 		public const float PiTimesPi = (float)(3.141592653589793239 * 3.141592653589793239);
 
 		public static readonly int[] Primes = {
-			3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
-			1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
-			17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363, 156437,
-			187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
-			1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369 };
+		3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
+		1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
+		17519, 21023, 25229, 30293, 36353, 43627, 52361, 62851, 75431, 90523, 108631, 130363, 156437,
+		187751, 225307, 270371, 324449, 389357, 467237, 560689, 672827, 807403, 968897, 1162687, 1395263,
+		1674319, 2009191, 2411033, 2893249, 3471899, 4166287, 4999559, 5999471, 7199369 };
 
 		/// <summary>
 		/// Branchless sign; returns 0 if value < 0 and 1 if value >= 0
@@ -234,39 +234,33 @@ namespace Lidgren.Core
 		}
 
 		/// <summary>
-		/// Euclidean Modulo (not just remainder)
+		/// Floored division modulo (not just remainder)
 		/// </summary>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static double Modulo(double value, double mod)
 		{
 			var m = value % mod;
-			if (m < 0)
-				m = (mod < 0) ? m - mod : m + mod;
-			return m;
+			return ((mod * m) >= 0.0) ? m : m + mod;
 		}
 
 		/// <summary>
-		/// Euclidean Modulo (not just remainder)
+		/// Floored division modulo (not just remainder)
 		/// </summary>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static float Modulo(float value, float mod)
 		{
 			var m = value % mod;
-			if (m < 0)
-				m = (mod < 0) ? m - mod : m + mod;
-			return m;
+			return ((mod * m) >= 0.0f) ? m : m + mod;
 		}
 
 		/// <summary>
-		/// Euclidean Modulo (not just remainder)
+		/// Floored division modulo (not just remainder)
 		/// </summary>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static int Modulo(int value, int mod)
 		{
 			var m = value % mod;
-			if (m < 0)
-				m = (mod < 0) ? m - mod : m + mod;
-			return m;
+			return ((mod * m) >= 0) ? m : m + mod;
 		}
 
 		/// <summary>
